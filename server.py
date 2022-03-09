4cb60fb726283f048019324a44369fae13daf39d
Sandra Trošina <pr.mish@gmail.com>
2022-03-09 08:37:33 +0200

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,12 @@ app = Flask(__name__)
 @app.route('/',methods = ['POST', 'GET'])
 def root():
     return render_template("index.html")
-    
+
+#Ceļš uz about
+@app.route('/about',methods = ['POST', 'GET'])
+def about():
+    return render_template("about.html")
+
 #Pārbaudes lapa, lai saprastu, ka kods vispār strādā
 @app.route('/health')
 def health():
