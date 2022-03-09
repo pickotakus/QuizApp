2145ef10fad72396c353ffec41ccb2117fddd9e8
Sandra Trošina <pr.mish@gmail.com>
2022-03-09 09:00:22 +0200

    Pielikām test.html
    Pielikām test.html

--- server.py
+++ server.py
@@ -14,6 +14,12 @@ def root():
 def about():
     return render_template("about.html")
 
+#Ceļš uz test
+@app.route('/test',methods = ['POST', 'GET'])
+def test():
+    parametri = ["IQ","Augums","Kājas izmērs"]
+    return render_template("test.html",parametri=parametri)
+
 #Pārbaudes lapa, lai saprastu, ka kods vispār strādā
 @app.route('/health')
 def health():
