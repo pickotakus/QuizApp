30a61e81b0580fc5d2befd70b52a1b6cb9fb42f2
Sandra Trošina <pr.mish@gmail.com>
2022-03-28 09:18:10 +0300

    123
    123

--- server.py
+++ server.py
@@ -13,11 +13,11 @@ def root():
 def about():
   return render_template("about.html")
 
-#Ceļš uz test
 @app.route('/test',methods = ['POST', 'GET'])
 def test():
     parametri = ["IQ","Augums","Kājas izmērs"]
-    return render_template("test.html",parametri=parametri)
+    images = ["https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d","https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-boy-cat-names-1606242656.jpg","https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1175550351.jpg"]
+    return render_template("test.html",parametri=parametri,images=images)
 
 #Pārbaudes lapa, lai saprastu, ka kods vispār strādā
 @app.route('/health')
