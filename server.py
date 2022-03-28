9aa399ac9e8bd3a780c536009ac25318623ff9bb
Sandra Trošina <pr.mish@gmail.com>
2022-03-28 10:42:03 +0300

    pielikām picDB
    pielikām picDB

--- server.py
+++ server.py
@@ -4,6 +4,21 @@ from flask import render_template
 
 app = Flask(__name__)
 
+def loadAllPictures():
+  filepath = 'picDB.txt'
+  pictures=[]
+  with open(filepath) as fp:
+    line = fp.readline()
+    cnt = 1
+    while line:
+      if line!='':
+        fullPicInfo = line.strip().split(';')
+        pictures.append(fullPicInfo[1])
+      line = fp.readline()
+      cnt += 1
+  fp.close()
+  return pictures
+
 #Pirmā lapa, kas tiks ielādēta
 @app.route('/',methods = ['POST', 'GET'])
 def root():
@@ -16,7 +31,7 @@ def about():
 @app.route('/test',methods = ['POST', 'GET'])
 def test():
     parametri = ["IQ","Augums","Kājas izmērs"]
-    images = ["https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d","https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-boy-cat-names-1606242656.jpg","https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1175550351.jpg"]
+    images = loadAllPictures()
     return render_template("test.html",parametri=parametri,images=images)
 
 #Pārbaudes lapa, lai saprastu, ka kods vispār strādā
